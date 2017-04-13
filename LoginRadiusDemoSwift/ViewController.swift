26af442653095bea23d2454bd824660db35fd7d5
ashwinishanbhag <ashwinishanbhag03@gmail.com>
2017-04-12 20:49:28 -0700

    Initial Commit

---
+++ LoginRadiusDemoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginRadiusDemoSwift
+//
+//  Created by Anaaya Nayanesh Acharya on 12/04/17.
+//  Copyright © 2017 Ashwini Acharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
